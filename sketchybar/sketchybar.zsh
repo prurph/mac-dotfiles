f36007c4e13e892a4ec7d0dbc7f0e8cb845005d8
Prescott Murphy <prurph@gmail.com>
2023-11-11 16:41:58 -0500

    Set item and plugin dirs in sketchybarrc

--- sketchybar/sketchybar.zsh
+++ sketchybar/sketchybar.zsh
@@ -1,5 +1,6 @@
 rm -rf ~/.config/sketchybar
-mkdir -p ~/.config/sketchybar
+mkdir -p ~/.config/sketchybar/{items,plugins}
 
 ln -sf ~/Dotfiles/sketchybar/sketchybarrc ~/.config/sketchybar/sketchybarrc
+ln -sf ~/Dotfiles/sketchybar/items/* ~/.config/sketchybar/items
 ln -sf ~/Dotfiles/sketchybar/plugins/* ~/.config/sketchybar/plugins
