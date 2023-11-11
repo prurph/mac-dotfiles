ca16e98154092d9551143e9ce17b8744d82464db
Prescott Murphy <prurph@gmail.com>
2023-11-11 13:18:28 -0500

    Add sketchybar with default config

---
+++ sketchybar/sketchybar.zsh
@@ -0,0 +1,5 @@
+rm -rf ~/.config/sketchybar
+mkdir -p ~/.config/sketchybar
+
+ln -sf ~/Dotfiles/sketchybar/sketchybarrc ~/.config/sketchybar/sketchybarrc
+ln -sf ~/Dotfiles/sketchybar/plugins/* ~/.config/sketchybar/plugins
