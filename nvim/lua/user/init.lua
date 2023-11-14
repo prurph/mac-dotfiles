8bdaa6365ef04038d036537c410436d68122ce62
Prescott Murphy <prurph@gmail.com>
2023-11-14 18:30:40 -0500

    Restyle statusline slightly and add S-hl mappings to navigate buffers

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -1,3 +1,13 @@
 return {
   colorscheme = "catppuccin",
+  -- TODO: beautify statusline
+  heirline = {
+    attributes = {
+      git_branch = { bold = false },
+      git_diff = { bold = false },
+    },
+    icon_highlights = {
+      breadcrumbs = true,
+    },
+  },
 }
--- nvim/lua/user/mappings.lua
+++ nvim/lua/user/mappings.lua
@@ -2,35 +2,40 @@ local nvim_tmux_navigation = require "nvim-tmux-navigation"
 
 return {
   n = {
+    -- Unmap the defaults to close all to left/right
+    ["<leader>bl"] = nil,
+    ["<leader>br"] = nil,
+    ["<S-h>"] = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Buffer to left",
+    },
+    ["<S-l>"] = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Buffer to right",
+    },
     ["<C-h>"] = {
       nvim_tmux_navigation.NvimTmuxNavigateLeft,
       desc = "Move to split left [P]",
-      silent = true,
     },
     ["<C-j>"] = {
       nvim_tmux_navigation.NvimTmuxNavigateDown,
       desc = "Move to split below [P]",
-      silent = true,
     },
     ["<C-k>"] = {
       nvim_tmux_navigation.NvimTmuxNavigateUp,
       desc = "Move to split below [P]",
-      silent = true,
     },
     ["<C-l>"] = {
       nvim_tmux_navigation.NvimTmuxNavigateRight,
       desc = "Move to split right [P]",
-      silent = true,
     },
     ["<C-\\>"] = {
       nvim_tmux_navigation.NvimTmuxNavigateLastActive,
       desc = "Move to last active split [P]",
-      silent = true,
     },
     ["<C-Space>"] = {
       nvim_tmux_navigation.NvimTmuxNavigateNext,
       desc = "Move to next split [P]",
-      silent = true,
     },
   },
 }
